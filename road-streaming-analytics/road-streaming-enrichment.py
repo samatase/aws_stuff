5576145d6ec71e9e5d460e6272c195694c860a9d
Samatas <samatas@8c859081789d.ant.amazon.com>
2018-08-31 17:00:04 +0100

    Changed lamdda function to use json

--- road-streaming-analytics/road-streaming-enrichment.py
+++ road-streaming-analytics/road-streaming-enrichment.py
@@ -1,34 +1,36 @@
 from __future__ import print_function
-import csv
-import datetime
+
 import base64
-import boto3
+import json
 
 print('Loading function')
-
+sensors = {
+    "asuua":{"lat":51.593178,"long":-0.169281,"name":"Leslie Road East Finchley"},
+    "geoir":{"lat":51.590407,"long":-0.164416,"name":"East Finchley High Street"},
+    "sdljj":{"lat":51.585661,"long":-0.163558,"name":"Great North Road"},
+    "vyucg":{"lat":51.581287,"long":-0.155579,"name":"Archway Road"},
+    "jbxup":{"lat":51.576967,"long":-0.145584,"name":"Highgate Road"}
+}
 
 def lambda_handler(event, context):
     output = []
-    now = datetime.datetime.now()
-
     for record in event['records']:
         payload = base64.b64decode(record['data'])
-        r = csv.reader([payload])
-        csvRecordList = list(r)
-        junctionId = csvRecordList[0][0]
-        timestamp = csvRecordList[0][1]
-        juctionBits = csvRecordList[0][2]
-        date = now.strftime("%d-%m-%Y")
-        timestamp = date + " " + timestamp
-        processedEvent = junctionId + "," + timestamp + "," + juctionBits
+        trafficEventJson = json.loads(payload)
+        sensorID = trafficEventJson['sensorId']
+        trafficEventJson['latitude'] = sensors[sensorID]['lat']
+        trafficEventJson['longitude'] = sensors[sensorID]['long']
+        trafficEventJson['locationName'] = sensors[sensorID]['name']
+        trafficEventJson['recordId'] = record['recordId']
+
+        enrichedEvent = json.dumps(trafficEventJson)
 
         output_record = {
             'recordId': record['recordId'],
             'result': 'Ok',
-            'data': base64.b64encode(processedEvent + "\n")
+            'data': base64.b64encode(enrichedEvent+"\n")
         }
         output.append(output_record)
-
     print('Successfully processed {} records.'.format(len(event['records'])))
-
     return {'records': output}
+
